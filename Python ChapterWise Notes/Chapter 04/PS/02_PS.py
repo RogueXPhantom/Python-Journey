675a2e0489020f48a697f0820c1c91ace2debe82
RogueXPhantom <pulkitgautam7@gmail.com>
2026-07-13 23:46:31 +0530

    changed a bit of code bcz used ai to write all those lines

--- Python ChapterWise Notes/Chapter 04/PS/02_PS.py
+++ Python ChapterWise Notes/Chapter 04/PS/02_PS.py
@@ -1,20 +1,20 @@
-marks = []
+Student = []
 
-m1 = int(input("Enter marks for subject 1: "))
-marks.append(m1)
+m1 = int(input("Enter marks of student 1 : "))
+Student.append(m1)
 
-m2 = int(input("Enter marks for subject 2: "))
-marks.append(m2)
+m2 = int(input("Enter marks of student 2 : "))
+Student.append(m2)
 
-m3 = int(input("Enter marks for subject 3: "))
-marks.append(m3)
-m4 = int(input("Enter marks for subject 4: "))
-marks.append(m4)
+m3 = int(input("Enter marks of student 3 : "))
+Student.append(m3)
+m4 = int(input("Enter marks of student 4 : "))
+Student.append(m4)
 
-m5 = int(input("Enter marks for subject 5: "))
-marks.append(m5)
+m5 = int(input("Enter marks of student 5 : "))
+Student.append(m5)
 
-m6 = int(input("Enter marks for subject 6: "))
-marks.append(m6)
+m6 = int(input("Enter marks of student 6 : "))
+Student.append(m6)
 
-print(sorted(marks))
+print(sorted(Student))
