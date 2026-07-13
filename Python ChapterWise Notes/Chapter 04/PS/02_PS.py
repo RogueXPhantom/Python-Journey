e328933e6899e8efdd7751b1b999ac1c8deb0d08
RogueXPhantom <pulkitgautam7@gmail.com>
2026-07-13 23:24:24 +0530

    added list modules and tuple example and practice set solution

---
+++ Python ChapterWise Notes/Chapter 04/PS/02_PS.py
@@ -0,0 +1,20 @@
+marks = []
+
+m1 = int(input("Enter marks for subject 1: "))
+marks.append(m1)
+
+m2 = int(input("Enter marks for subject 2: "))
+marks.append(m2)
+
+m3 = int(input("Enter marks for subject 3: "))
+marks.append(m3)
+m4 = int(input("Enter marks for subject 4: "))
+marks.append(m4)
+
+m5 = int(input("Enter marks for subject 5: "))
+marks.append(m5)
+
+m6 = int(input("Enter marks for subject 6: "))
+marks.append(m6)
+
+print(sorted(marks))
